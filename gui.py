dd844eaae0cd0dea51e5a2951a5f59785a6c28f4
LUKE GIDLEY <18089236@students.latrobe.edu.au>
2019-08-26 13:24:11 +1000

    Improved GUI and added functionality to change frames

--- gui.py
+++ gui.py
@@ -68,14 +68,16 @@ class Deploy(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
+		
+        for F in (SimpleMode, ExpertMode):
+        
+        	frame = F(container, self)
 
-        frame = SimpleMode(container, self)
+        	self.frames[F] = frame
 
-        self.frames[SimpleMode] = frame
+        	frame.grid(row=0, column=0, sticky="nsew")
 
-        frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(SimpleMode)
+        	self.show_frame(SimpleMode)
 
     def show_frame(self, cont):
 
@@ -93,24 +95,27 @@ class SimpleMode(tk.Frame):
         label = tk.Label(self, text="PotentioStat", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         
-        
+        colourLabelY = tk.Label(self, background="#326ada", width=5, height=16)
+        colourLabelY.grid(column=0)
+
+        colourLabelX = tk.Label(self, background="#326ada", width=54, height=2)
+        colourLabelX.grid(column=1, row=4)
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.get_tk_widget().grid(row=2, column=1)
 
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas._tkcanvas.grid(row=2, column=1)
+        #canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        button2 = ttk.Button(self, text="Toggle Demo", command=testEngine.dToggle)
-        button2.pack()
-        button1 = ttk.Button(self, text="Calibrate and go")
-        button1.pack()
+        calibrateButton = ttk.Button(self, text="Toggle Demo", command=testEngine.dToggle)
+        button2.grid(column=3, row=4)
+        buttonExpertMode = ttk.Button(self, text="Expert Mode", command=lambda: controller.show_frame(ExpertMode))
+        buttonExpertMode.grid(column=3, row=1)
         
-        
-        
-        
+
 
 class ExpertMode(tk.Frame):
 
@@ -119,18 +124,89 @@ class ExpertMode(tk.Frame):
         label = tk.Label(self, text="Potentio Stat", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         
-        f = Figure(figsize=(5,5), dpi = 100)
-        a = f.add_subplot(111)
-        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        colourLabelY = tk.Label(self, background="#326ada", width=3, height=18)
+        colourLabelY.grid(column=0, rowspan=6)
 
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        colourLabelX = tk.Label(self, background="#326ada", width=57, height=2)
+        colourLabelX.grid(column=1, row=6, columnspan=5)
 
-        button1 = ttk.Button(self, text="Calibrate and go")
-        button1.pack()
+        label1 = tk.Label(self, text = "DAC Offset")
+        label1.grid(column=1, row = 0, padx=3, pady=3 )
+
+        label1 = tk.Label(self, text = "DAC Gain")
+        label1.grid(column=1, row = 1, padx=3, pady=3  )
+
+        label1 = tk.Label(self, text = "Pot. Offset")
+        label1.grid(column=1, row = 2, padx=3, pady=3  )
+
+        label1 = tk.Label(self, text = "Curr. Offset")
+        label1.grid(column=1, row = 3, padx=3, pady=3  )
+
+        label1 = tk.Label(self, text = "R1")
+        label1.grid(column=1, row = 4, padx=3, pady=3 )
+
+        label1 = tk.Label(self, text = "R2")
+        label1.grid(column=3, row = 0, padx=3, pady=3  )
+
+        label1 = tk.Label(self, text = "R3")
+        label1.grid(column=3, row = 1, padx=3, pady=3 )
+
+        label1 = tk.Label(self, text = "Current Range")
+        label1.grid(column=3, row = 2, padx=3, pady=3  )
+
+        label1 = tk.Label(self, text = "Potential(V)")
+        label1.grid(column=3, row = 3, padx=3, pady=3  )
+
+        label1 = tk.Label(self, text = "PID")
+        label1.grid(column=3, row = 4, padx=3, pady=3  )
+
+        entry1 = tk.Entry(self)
+        entry1.grid(column=2, row = 0)
+
+        entry2 = tk.Entry(self)
+        entry2.grid(column=2, row = 1)
+
+        entry3 = tk.Entry(self)
+        entry3.grid(column=2, row = 2)
+
+        entry4 = tk.Entry(self)
+        entry4.grid(column=2, row = 3)
+
+        entry5 = tk.Entry(self)
+        entry5.grid(column=2, row = 4)
+
+        entry6 = tk.Entry(self)
+        entry6.grid(column=4, row = 0)
+
+        entry7 = tk.Entry(self)
+        entry7.grid(column=4, row = 1)
+
+        entry8 = tk.Entry(self)
+        entry8.grid(column=4, row = 2)
+
+        entry9 = tk.Entry(self)
+        entry9.grid(column=4, row = 3)
+
+        entry10 = tk.Entry(self)
+        entry10.grid(column=4, row = 4)
+
+        buttonSimpleMode = ttk.Button(self, text="Simple", command=lambda: controller.show_frame(SimpleMode))
+        buttonSimpleMode.grid(column=6, row=0)
+
+        applyVariables= ttk.Button(self, text="Apply")
+        applyVariables.grid(column=6, row =6)
         
-
+		def get_parameters():
+        entry1Data = entry1.get()
+        entry2Data = entry2.get()
+        entry3Data = entry3.get()
+        entry4Data = entry4.get()
+        entry5Data = entry5.get()
+        entry6Data = entry6.get()
+        entry7Data = entry7.get()
+        entry8Data = entry8.get()
+        entry9Data = entry9.get()
+        entry10Data = entry10.get()
 
 class UploadData(tk.Frame):
 
