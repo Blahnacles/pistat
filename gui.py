7a972811a7813b58517e1697f3be852882929859
LUKE GIDLEY <18089236@students.latrobe.edu.au>
2019-08-26 14:26:54 +1000

    Fixed final .pack issues

--- gui.py
+++ gui.py
@@ -212,38 +212,38 @@ class UploadData(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Potentio Stat", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        #label = tk.Label(self, text="Potentio Stat", font=LARGE_FONT)
+        #label.pack(pady=10,padx=10)
         
-        f = Figure(figsize=(5,5), dpi = 100)
-        a = f.add_subplot(111)
-        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        #f = Figure(figsize=(5,5), dpi = 100)
+        #a = f.add_subplot(111)
+        #a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
 
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        #canvas = FigureCanvasTkAgg(f, self)
+        #canvas.draw()
+        #canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        button1 = ttk.Button(self, text="Calibrate and go")
-        button1.pack()
+        #button1 = ttk.Button(self, text="Calibrate and go")
+        #button1.pack()
 
 class Test1Mode(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Potentio Stat", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        label.grid(column=1, row=1)
         
         
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.get_tk_widget().grid(column=2, row=2)
 
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas._tkcanvas.grid(column=2, row=2)
 
         button1 = ttk.Button(self, text="Calibrate and go")
-        button1.pack()
+        button1.grid(column=3, row=3)
 
 
 
