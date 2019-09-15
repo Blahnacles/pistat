319d94b05e1d3867db735859a0e9d901ef1b306d
Simon <18774937@students.latrobe.edu.au>
2019-09-15 22:47:53 +1000

    TEAMS-219 debug prints to determine cause

--- deviceConfig.py
+++ deviceConfig.py
@@ -500,7 +500,7 @@ class UsbStat:
             return p,i
         return None, None
     def vOutput(self, value=1):
-        print(self.potential_offset)
+        print("poffset:",self.potential_offset)
         self.send_command(b'DACSET '+self.ddb(value/8.*2.**19+int(round(self.potential_offset/4.))),b'OK')
 
     def ddb(self,v):
